b7d341d9aa45129f25a42f56c485926fbdbe37a9
Gustavo Gomes <gustavogomestome509@gmail.com>
2024-11-27 23:19:18 -0300

    Primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,27 @@
+var elementosDuvida = document.querySelectorAll(".duvida");
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+
+
+
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener("click", function () {
+    duvida.classList.toggle("ativa");
+  });
+});
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+})
